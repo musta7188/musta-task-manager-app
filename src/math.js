48da15207e7d6863bc7192afc7f0df323ceb0e5a
musta7188 <musta7188@hotmail.it>
2020-09-16 12:25:24 +0100

    testing implmented in direcotry

--- src/math.js
+++ src/math.js
@@ -1,10 +1,22 @@
-const calculateTips = (total, percentageTip) =>{
-  const tips = total * percentageTip
-  return total + tips
+const calculateTips = (total, percentageTip = .25) =>{ return total + (total * percentageTip)
 }
+
+const fahrenheitToCelsius = (temp) => {
+  return (temp - 32) / 1.8
+}
+
+const celsiusToFahrenheit = (temp) => {
+  return (temp * 1.8) + 32
+}
+
+
+
 
 
 
 module.exports = {
   calculateTips,
+  fahrenheitToCelsius,
+  celsiusToFahrenheit
 }
+
--- test/math.test.js
+++ test/math.test.js
@@ -1,3 +1,51 @@
-const {calculateTips} = require('../src/math')
+const {
+  calculateTips,
+  fahrenheitToCelsius,
+  celsiusToFahrenheit
+
+} = require('../src/math')
+
+
+test('should calculate total with tips', () =>{
+
+  const total  = calculateTips(10, 0.30) 
+
+  expect(total).toBe(13)
+  
+
+})
+
+test('should calculate total with default tips', () =>{
+  const total = calculateTips(10)
+  expect(total).toBe(12.5)
+})
+
+
+test('convert fahrenheit 32 to celsius 0', () =>{
+  const temperature  = fahrenheitToCelsius(32)
+  expect(temperature).toBe(0)
+})
+
+
+test('convert Celsius 0 to be Fahrenheit 32 ', () =>{
+  const temperature = celsiusToFahrenheit(0)
+
+  expect(temperature).toBe(32)
+})
+
+
+
+
+
+
+//  why test?
+
+// --save time 
+// --create reliable software
+// --gives flexibility to develop
+//     -refactoring 
+//     -Collaborating
+//     -Profiling
+// --Peace of mind
 
 
